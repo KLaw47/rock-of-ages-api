e140055a49fca003530a618f40718028e36d31b6
Keaton Law <keaton.c.law@gmail.com>
2025-01-03 17:01:39 -0600

    adaptive list method

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -38,12 +38,26 @@ class RockView(ViewSet):
         Returns:
             Response -- JSON serialized array
         """
-        try:
+        # Get query string parameter
+        owner_only = self.request.query_params.get("owner", None)
+        try: 
             rocks = Rock.objects.all()
+
+            #if query owner=current is in url
+            if owner_only is not None and owner_only == "current":
+                #then filter the rocks
+                rocks = rocks.filter(user=request.auth.user)
+
             serializer = RockSerializer(rocks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+        # try:
+        #     rocks = Rock.objects.all()
+        #     serializer = RockSerializer(rocks, many=True)
+        #     return Response(serializer.data, status=status.HTTP_200_OK)
+        # except Exception as ex:
+        #     return HttpResponseServerError(ex)
     
     def destroy(self, request, pk=None):
         """Handle DELETE requests for a single item
